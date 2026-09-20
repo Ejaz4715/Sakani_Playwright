2c159ada6ab24a8e9812b04db3d51c47f25c8cf3
ejaz4715 <ejazmd15@hotmail.com>
2026-09-20 17:44:22 +0300

    updated name of objects folder

---
+++ src/objects/PaymentConfirmationObjects.ts
@@ -0,0 +1,4 @@
+export const PaymentConfirmationObjects = {
+  closeButton: { role: "button", name: "إغلاق" },
+  successMessage: "h4",
+} as const;
---
+++ src/objects/ProjectUnitsObjects.ts
@@ -0,0 +1,5 @@
+export const ProjectUnitsObjects = {
+  propertyText: "شقة",
+  landUnitCard: "//app-dx-project-unit-card",
+  unitCard: "//app-marketplace-unit-card/descendant::span[text() ='SAR']",
+} as const;
