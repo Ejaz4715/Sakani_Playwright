32461c057d4a4914ed11ecd3d02a97450d74de6f
ejaz4715 <ejazmd15@hotmail.com>
2026-09-21 14:02:39 +0300

    Add objects

---
+++ src/objects/MarketplaceLandingObjects.ts
@@ -0,0 +1,8 @@
+export const MarketplaceLandingObjects = {
+  searchButton: { role: "button", name: "بحث", exact: true },
+  searchInput: {
+    role: "textbox",
+    name: "ماذا تبحث عنه؟ العقارات أو الخدمات أو الدعم",
+  },
+  projectResultModal: "app-global-search-modal-result-block",
+} as const;
